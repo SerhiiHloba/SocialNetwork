6707f59551d5e490a4b9ff1a93a416ad40e8c674
Globa S <globasergey29@gmail.com>
2020-01-30 16:09:57 +0200

    Add project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+        <img src="http://ua.seo-engines.com/wp-content/uploads/logo_illustration_05_upload.jpg" />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+        <div>
+            <img src="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+        </div>
+        <div>
+            ava + desc
+    </div>
+        <div>
+            My posts
+      <div>
+                New posts
+      </div>
+            <div>
+                <div>
+                    post1
+        </div>
+                <div>
+                    post2
+        </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
